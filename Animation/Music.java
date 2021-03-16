8f1fb28cd41a2c05a1c396afb191bdcebb6f430c
chrislvr <cmayika17@gmail.com>
2021-03-16 09:38:43 +0100

    Add files via upload

---
+++ Animation/Music.java
@@ -0,0 +1,25 @@
+import java.io.File;
+
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+public class Music extends Medias {
+
+
+public void play1() {
+	    player.play();
+	 }
+public void stop1() {
+	player.stop();
+}
+public void ready() {
+	ready.play();
+}
+public void lvl1() {
+	niv1.play();
+}
+public void congrats() {
+	congratulations.play();
+}
+}
+
