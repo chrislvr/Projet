b37cdfa8ee9b6c2b90531aa0e16afde59b215dd5
Florian CASAGRANDE <60674214+w0lfnix@users.noreply.github.com>
2021-03-16 13:07:39 +0100

    Delete MainAffichageNiveau.java

--- MainAffichageNiveau.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.io.IOException;
-
-public class MainAffichageNiveau {
-    public static void main(String[] args) throws IOException {
-        Stage s = new Stage(new File("./Ressources/Niveau1.txt"));
-        s.toFrame().show();
-    }
-}
