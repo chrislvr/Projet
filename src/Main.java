cb19109c6d743479392b308d0672ab14c5add0a5
Florian CASAGRANDE <60674214+w0lfnix@users.noreply.github.com>
2021-03-23 13:13:10 +0100

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Stage s = new Stage(new File("./Ressources/Stages/stage1.txt"));
-        s.toFrame().show();
-    }
-}
